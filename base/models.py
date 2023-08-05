c9d2c189efab9c2a975ebb22b8c7830db3a7399c
Manish <manishdwivedi45165@gmail.com>
2023-08-05 16:11:32 +0530

    Created login functunality along with registration form

--- base/models.py
+++ base/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
+
 from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Topic(models.Model):
